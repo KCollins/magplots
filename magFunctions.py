7edc107f93223fa4d1b462adee0dc0cd4633dd7a
KCollins <kvc2@case.edu>
2024-02-06 01:21:50 -0500

    Added initial versions of `magfetch` and `magfetchtgo`.

--- magFunctions.py
+++ magFunctions.py
@@ -5,6 +5,15 @@
 from scipy import interpolate
 import numpy as np
 
+
+# For pulling data from CDAweb:
+from ai import cdas
+import datetime
+from matplotlib import pyplot as plt
+import matplotlib as mpl
+
+import pandas as pd
+
 ############################################################################################################################### 
 
 # #  FILL_NAN: Function to eliminate NaN values from a 1D numpy array.
@@ -58,5 +67,106 @@ def reject_outliers(y):   # y is the data in a 1D numpy array
         final_list[n] = y[n] if y[n] > mean - 3 * sd else np.nan
         final_list[n] = final_list[n] if final_list[n] < mean + 5 * sd else np.nan
     return final_list
+
+############################################################################################################################### 
+
+def magfetchtgo(start, end, magname, tgopw = '', resolution = '1sec'):
+    """
+    Pulls data from a RESTful API with a link based on the date.
+
+    Args:
+        start (datetime.datetime): The start date of the data to be fetched.
+        end (datetime.datetime): The end date of the data to be fetched.
+        magname (str): The name of the magnetometer station.
+        tgopw (str): Password for Tromsø Geophysical Observatory.
+        resolution (str): String for data resolution; e.g., '10sec'; default '1sec'
+
+    Returns:
+        pandas.DataFrame: A pandas DataFrame containing the fetched data.
+    """
+    if(tgopw == ''):
+        print("No password given; cannot pull data from Tromsø Geophysical Observatory. Save a password locally in tgopw.txt.")
+    
+    df = pd.DataFrame()
+
+    # Loop over each day from start to end
+    for day in range(start.day, end.day + 1):
+        # Generate the URL for the current day
+        url = f'https://flux.phys.uit.no/cgi-bin/mkascii.cgi?site={magname}4d&year={start.year}&month={start.month}&day={day}&res={resolution}&pwd='+ tgopw + '&format=XYZhtml&comps=DHZ&getdata=+Get+Data'
+
+        # Fetch the data for the current day
+        foo = pd.read_csv(url, skiprows = 6, delim_whitespace=True, usecols=range(5), index_col=False)
+        # Convert the 'DD/MM/YYYY HH:MM:SS' column to datetime format
+        foo['DD/MM/YYYY HH:MM:SS'] = foo['DD/MM/YYYY'] + ' ' + foo['HH:MM:SS']
+        foo['UT'] = pd.to_datetime(foo['DD/MM/YYYY HH:MM:SS'], format='%d/%m/%Y %H:%M:%S')
+        foo = foo[(foo['UT'] >= start) & (foo['UT'] <= end)] # remove values before start, after end
+        # foo['UT'] = foo['UT'].to_pydatetime()
+        # Rename the columns
+        foo.rename(columns={'X': 'MAGNETIC_NORTH_-_H', 'Y': 'MAGNETIC_EAST_-_E', 'Z': 'VERTICAL_DOWN_-_Z'}, inplace=True)
+        df = pd.concat([df, foo])
+
+    # # Convert the dataframe to a dictionary
+    data = {
+        'UT': df['UT'].to_numpy(),
+        'MAGNETIC_NORTH_-_H': df['MAGNETIC_NORTH_-_H'].to_numpy(),
+        'MAGNETIC_EAST_-_E': df['MAGNETIC_EAST_-_E'].to_numpy(),
+        'VERTICAL_DOWN_-_Z': df['VERTICAL_DOWN_-_Z'].to_numpy()
+    }
+    
+    # Convert 'UT' column to datetime64[ns] array
+    data['UT'] = pd.to_datetime(data['UT'], format='%Y-%m-%dT%H:%M:%S.%f')
+
+    # Round 'UT' column to microsecond precision
+    data['UT'] = data['UT'].round('us')
+
+    # Convert 'UT' column to datetime objects
+    data['UT'] = data['UT'].to_pydatetime()
+    # print(type(df))
+    # return df
+    return data
+
+############################################################################################################################### 
+
+
+def magfetch(
+    start = datetime.datetime(2016, 1, 24, 0, 0, 0), 
+    end = datetime.datetime(2016, 1, 25, 0, 0, 0), 
+    magname = 'atu', 
+    is_verbose = False, 
+    tgopw = '',
+    resolution = '1sec'
+):
+    """
+    MAGFETCH 
+        Function to fetch data for a given magnetometer. Pulls from ai.cdas or DTU.
+
+        Arguments:
+            start, end   : datetimes of the start and end of sampled data range.
+            magname      : IAGA ID for magnetometer being sampled. e.g.: 'upn'
+            is_verbose   : Boolean for whether debugging text is printed.
+            tgopw        : Password for Tromsø Geophysical Observatory
+            resolution   : Data resolution for TGO data.
+
+        Returns:
+            df           : pandas dataframe with columns ['UT', 'MAGNETIC_NORTH_-_H', 'MAGNETIC_EAST_-_E', 'VERTICAL_DOWN_-_Z']
+    """
+    # Pull password for TGO from local .txt file:
+    file = open("tgopw.txt", "r")
+    tgopw = file.read()
+    if(is_verbose): print('Found Tromsø Geophysical Observatory password.')
+    if(magname in ['upn', 'umq', 'gdh', 'atu', 'skt', 'ghb']): 
+        if(is_verbose): print('Collecting data for ' + magname + ' from TGO.')
+        if(end<start): print('End is after start... check inputs.')
+        data = magfetchtgo(start, end, magname, tgopw = tgopw, resolution = resolution)
+    else:
+        data = cdas.get_data(
+            'sp_phys',
+            'THG_L2_MAG_'+ magname.upper(),
+            start,
+            end,
+            ['thg_mag_'+ magname]
+        )
+    if(is_verbose): print('Data for ' + magname + ' collected: ' + str(len(data['UT'])) + ' samples.')
+    return data
 
 ############################################################################################################################### 
