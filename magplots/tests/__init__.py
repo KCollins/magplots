891a476ec6087d489a44f1cf32f629c833bade17
KCollins <kvc2@case.edu>
2024-10-23 15:08:30 -0400

    WIP on #50

---
+++ magplots/tests/__init__.py
@@ -0,0 +1,8 @@
+"""Unit and Integration Tests for magplots.
+
+Note
+----
+This file must remain empty of code for Python tests to function.
+
+
+"""
---
+++ magplots/tests/test_magplots.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# Full license can be found in License.md
+# Full author list can be found in .zenodo.json file
+# DOI:10.5281/zenodo.10703227
+# ----------------------------------------------------------------------------
+"""Integration and unit test suite for magplots methods."""
+
+import datetime
+import unittest
+
+import magplots.magFunctions as mp
+
+
+class TestFindConjugate(unittest.TestCase):
+    """Integration and unit test suite for conj_calc methods."""
+
+    def setUp(self):
+        """Initialize the test case by copying over necessary files."""
+        self.start = datetime.datetime(2018, 9, 4, 0, 0, 0)
+        self.end = datetime.datetime(2018, 9, 5, 0, 0, 0)
+        self.magname = "atu"  
+        self.resolution = "1sec"
+
+    def tearDown(self):
+        """Clean up the test environment."""
+        del self.ut, self.lat, self.lon, self.method, self.expected_lat
+        del self.expected_lon
+
+    def eval_magfetch(self):
+        """Evaluate the `magfetch` function."""
+        result = magfetch(self.start, self.end, self.magname, self.resolution)
+       
+
+        # Assert that the returned values are close enough to expected values
+        self.assertAlmostEqual(result["UT"][1],  datetime(2018, 9, 4, 0, 0, 1, 1000), delta=1)
+
